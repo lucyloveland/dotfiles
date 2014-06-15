71a215407fa668f65b5bee2c59b94934f71ff732
jloveland <jason.c.loveland@gmail.com>
2014-06-14 23:21:06 -0400

    adding atom configs and separating Casks into Caskfile

--- atom/install.sh
+++ atom/install.sh
@@ -2,10 +2,23 @@
 packages="
   pretty-json
   coffee-lint
+  monokai
 "
 
 for package in $packages
 do
   echo "Installing Atom package: "$package
   apm install $package
+done
+
+# Remove all the files that will get symlinked with fresh
+FILES=~/.dotfiles/atom/*
+for f in $FILES
+do
+  if [[ $f != *install.sh ]]
+    then
+      file=${f##*/}
+      rm -f ~/.atom/$file
+      ln -s ~/.dotfiles/atom/$file ~/.atom/$file
+  fi
 done
