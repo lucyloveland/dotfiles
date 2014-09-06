b8d80146541d963b1d82775931a279a969df6645
jloveland <jason.c.loveland@gmail.com>
2014-09-05 23:18:41 -0400

    fixed ruby install script, removed sublime, and added vagrant install

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,6 +5,15 @@ set -ex
 export RUBY_VERSION='2.1.1'
 export CFLAGS=-Wno-error=shorten-64-to-32
 
-rbenv install $RUBY_VERSION
-rbenv global $RUBY_VERSION
-rbenv rehash
+INSTALLED_VERSION=$(cat ~/.rbenv/version)
+
+if [ $INSTALLED_VERSION != $RUBY_VERSION ]
+  then
+    rbenv install $RUBY_VERSION
+    rbenv global $RUBY_VERSION
+    rbenv rehash
+    ruby -v
+  else
+    echo "Ruby $RUBY_VERSION already installed unsing rbenv!"
+    ruby -v
+fi
---
+++ vagrant/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Chef
+vagrant plugin install vagrant-berkshelf
+vagrant plugin install vagrant-butcher
+vagrant plugin install vagrant-omnibus
+vagrant plugin install vagrant-ohai
+
+# providers
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-digitalocean
