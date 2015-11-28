3b0d5ef543f826dbe9955548860e7434f9376738
jloveland <jason.c.loveland@gmail.com>
2015-11-28 18:17:59 -0500

    adding nvm version set to v0.12.7

--- shell/aliases/nvm.sh
+++ shell/aliases/nvm.sh
@@ -2,5 +2,5 @@ export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 
 # set nvm to v0.12.7
-# export NODE_VERSION=v0.12.7
-# nvm use $NODE_VERSION
+export NODE_VERSION=v0.12.7
+nvm use $NODE_VERSION
