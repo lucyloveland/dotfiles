7d20466dbe8c5a7405e71188cc71abd0edf9749f
jloveland <jason.c.loveland@gmail.com>
2020-09-28 23:32:34 -0700

    Using pyenv instead of hard coding alias

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,5 @@
 sudo easy_install SpoofMAC
+
+export PYTHON_VERSION=3.8.5
+pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
--- shell/aliases/python.sh
+++ shell/aliases/python.sh
@@ -1,5 +1,11 @@
 alias prettyjson='python -m json.tool'
 
 # Python3
-alias python=/usr/local/bin/python3
-alias pip=/usr/local/bin/pip3
+# alias python=/usr/local/bin/python3
+# alias pip=/usr/local/bin/pip3
+
+# Use pyenv instead of hardcoding alias ^
+# set version in install script
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
