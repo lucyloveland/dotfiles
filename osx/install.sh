ad58e2b6f1b5f4cde83933575fc7d6b2e3407602
jloveland <jason.c.loveland@gmail.com>
2021-09-04 13:06:04 -0700

    Updating xcode install script

--- osx/install.sh
+++ osx/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 installed=$(xcode-select -p)
 
-if [[ $installed == "/Applications/Xcode.app/Contents/Developer" || $installed == $DEVELOPER_DIR ]]
+if [[ $installed == "/Library/Developer/CommandLineTools" || $installed == $DEVELOPER_DIR ]]
   then
     echo "xcode command line tools already installed..."
   else
