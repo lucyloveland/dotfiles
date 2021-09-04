59bc3f5e68fbecc0cb5612755b57be0472e87ff4
jloveland <jason.c.loveland@gmail.com>
2021-09-04 10:53:58 -0700

    Updating Python install

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,5 @@
 sudo easy_install SpoofMAC
 
-export PYTHON_VERSION=3.8.5
+export PYTHON_VERSION=3.9.6
 pyenv install $PYTHON_VERSION
 pyenv global $PYTHON_VERSION
--- shell/aliases/python.sh
+++ shell/aliases/python.sh
@@ -1,11 +1,14 @@
 alias prettyjson='python -m json.tool'
 
-# Python3
-# alias python=/usr/local/bin/python3
-# alias pip=/usr/local/bin/pip3
+# Alias to Python to the Python3 installed by Homebrew
+alias python=/usr/local/bin/python3
+alias pip=/usr/local/bin/pip3
 
-# Use pyenv instead of hardcoding alias ^
+# suppress homebrew warnings
+# alias brew='env PATH="${PATH//$(pyenv root)/shims:/}" brew'
+
 # set version in install script
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 fi
