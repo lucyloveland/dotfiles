50fb35eb88da345d8fb3707dc95910f4b0f0b523
lucyloveland <lucy.j.loveland@icloud.com>
2026-01-02 17:01:13 -0500

    Updating nvm

--- shell/aliases/nvm.sh
+++ shell/aliases/nvm.sh
@@ -1,6 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
 
 # Tell nvm to use latest node version installed as default
 nvm alias default node
